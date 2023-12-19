11a2d04d838277f9f5c3a8704d4035589d4f1880
SivakumarRajagopal <sivakumar07civil@gmail.com>
2023-12-19 07:55:41 +0000

    commit message

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -17,18 +17,7 @@ const NavBar = props => {
         <div className="cart-container">
           <p className="my-order-menu">My Orders</p>
           <div className="cart-icon-container">
-            <AiOutlineShoppingCart size={28} className="cart-icon" />
-            <p className="cart-orders">{cartItems}</p>
-          </div>
-        </div>
-      </div>
-
-      {/* For Mobile View */}
-      <div className="nav-mobile-container">
-        <h1 className="web-logo-heading">UNI Resto Cafe</h1>
-        <div className="cart-container">
-          <div className="cart-icon-container">
-            <AiOutlineShoppingCart size={20} className="cart-icon" />
+            <AiOutlineShoppingCart size={24} className="cart-icon" />
             <p className="cart-orders">{cartItems}</p>
           </div>
         </div>
